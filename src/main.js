70bd419fed0789e2f3b48f66193db2245a5565fd
Neycha <regentmarysapro@gmail.com>
2024-01-16 12:49:36 +0100

    Clean code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import './main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
